dbd606424439a5ad7886dcf55e304214d91650b6
= <=>
2024-06-03 18:36:46 +0300

    settings

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn --bind 127.0.0.1:8000 fox7.wsgi
+gunicorn --bind 0.0.0.0:8000 fox7.wsgi
 
 #--permit-unconventional-http-version
 
